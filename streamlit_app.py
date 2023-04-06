3680045cdcc5ba100c5d02a711ada2c0df0cfc87
Evgueni1234 <130084978+Evgueni1234@users.noreply.github.com>
2023-04-06 16:05:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-    return my_curr.fetchall()
+    return my_cur.fetchall()
     
 
 streamlit.title("My Parents' New Healthy Diner")
