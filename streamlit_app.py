6f390455b04442c297379c4d28a563503cd12ca7
Evgueni1234 <130084978+Evgueni1234@users.noreply.github.com>
2023-04-06 16:01:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit
 import pandas 
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 streamlit.title("My Parents' New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -18,15 +23,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# normalize json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output to screen
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Choose a fruit")
+  else:
+    
+    streamlit.dataframe(get_fruityvice_data(fruit_choice))
+except URLError as e:
+  streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -37,4 +42,4 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.multiselect("What fruit would you like to add", list(my_fruit_list.index))
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
+streamlit.stop()
