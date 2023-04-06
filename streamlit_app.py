dcaf423550f841117b71c050e25a0446b774e295
Evgueni1234 <130084978+Evgueni1234@users.noreply.github.com>
2023-04-06 16:04:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,12 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_curr.fetchall()
+    
+
 streamlit.title("My Parents' New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -33,12 +39,15 @@ try:
 except URLError as e:
   streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+
+
+
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+
 
 add_my_fruit = streamlit.multiselect("What fruit would you like to add", list(my_fruit_list.index))
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
